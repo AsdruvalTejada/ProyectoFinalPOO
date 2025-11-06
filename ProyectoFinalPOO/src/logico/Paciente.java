8cedd63bf28b1dade9eef1e70d63864cb73b2812
Wilmary Hernandez <WSHH0001@ce.pucmm.edu.do>
2025-11-06 15:23:54 -0400

    Completando campos

--- ProyectoFinalPOO/src/logico/Paciente.java
+++ ProyectoFinalPOO/src/logico/Paciente.java
@@ -1,5 +1,31 @@
 package logico;
 
 public class Paciente {
+	private String Nombre;
+	private String Apellido;
+	
+	public Paciente(String nombre, String apellido) {
+		super();
+		Nombre = nombre;
+		Apellido = apellido;
+	}
 
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellido() {
+		return Apellido;
+	}
+
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	
+	
+	
 }
