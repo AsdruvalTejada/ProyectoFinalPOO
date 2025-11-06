3f9a5f93ab7afb397736da6d33d5e5077e26d772
conil <conil@LAPTOP-7K0QEIBH.mshome.net>
2025-11-06 15:31:08 -0400

    estamos trabajando

---
+++ ProyectoFinalPOO/src/logico/Consulta.java
@@ -0,0 +1,19 @@
+package logico;
+
+public class Consulta {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Consulta(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+	
+}
