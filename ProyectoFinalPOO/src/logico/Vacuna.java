9a4de5b609a13366f5932f0ea4375dc1fe6f8928
Angel Belliard <atbc0002@ce.pucmm.edu.do>
2025-11-11 13:01:43 -0400

    Clase Enfermedad

---
+++ ProyectoFinalPOO/src/logico/Vacuna.java
@@ -0,0 +1,25 @@
+package logico;
+
+public class Vacuna {
+	private String id;
+	private String nombre;
+	
+	public Vacuna(String id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
