214629f2973fa63716c23902ae5b4a61f43f289f
Wilmary Hernandez <Wilmary Hernandez@DESKTOP-1HPVPUQ>
2025-11-12 23:00:20 -0400

    implementacion de la clase Sistema de Gestion y metodos de la clase Persona, Paciente y Consulta

--- ProyectoFinalPOO/src/logico/Paciente.java
+++ ProyectoFinalPOO/src/logico/Paciente.java
@@ -4,15 +4,16 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Paciente extends Persona{
+public class Paciente extends Persona {
+	
 	private ArrayList<Consulta> historialConsultas;
 	private HashMap<Vacuna, Boolean> registroVacunacion;
 	
 	public Paciente(String id, String name, String apellido, LocalDate fechaNacimiento, String sexo, String contacto) {
 		super(id, name, apellido, fechaNacimiento, sexo, contacto);
 		
-		historialConsultas = new ArrayList<>();
-		registroVacunacion = new HashMap<>();
+		this.historialConsultas = new ArrayList<>();
+		this.registroVacunacion = new HashMap<>();
 	}
 
 	public ArrayList<Consulta> getHistorialConsultas() {
@@ -22,7 +23,7 @@ public class Paciente extends Persona{
 	public void setHistorialConsultas(ArrayList<Consulta> historialConsultas) {
 		this.historialConsultas = historialConsultas;
 	}
-
+	
 	public HashMap<Vacuna, Boolean> getRegistroVacunacion() {
 		return registroVacunacion;
 	}
@@ -30,5 +31,78 @@ public class Paciente extends Persona{
 	public void setRegistroVacunacion(HashMap<Vacuna, Boolean> registroVacunacion) {
 		this.registroVacunacion = registroVacunacion;
 	}
+
+	public void inicializarRegistroVacunas(ArrayList<Vacuna> catalogoGeneral) {
+		this.registroVacunacion.clear();
+		for (Vacuna vacuna : catalogoGeneral) {
+			this.registroVacunacion.put(vacuna, false);
+		}
+	}
+
+	public void agregarConsulta(Consulta nuevaConsulta) {
+		if (nuevaConsulta != null) {
+			this.historialConsultas.add(nuevaConsulta);
+		}
+	}
+
+	public boolean marcarVacunaAplicada(Vacuna vacuna) {
+		if (this.registroContieneVacuna(vacuna)) {
+			this.registroVacunacion.put(vacuna, true);
+			return true;
+		}
+		return false; 
+	}
+
+	public ArrayList<Consulta> generarResumenHistorial() {
+		ArrayList<Consulta> resumen = new ArrayList<>();
+		
+		for (Consulta consulta : this.historialConsultas) {
+			
+			boolean esImportante = consulta.isEsImportanteParaResumen(); 
+			
+			boolean esVigilancia = false;
+			if (consulta.getDiagnostico() != null) {
+				esVigilancia = consulta.getDiagnostico().getEstaBajoVigilancia(); 
+			}
+
+			if (esImportante || esVigilancia) {
+				resumen.add(consulta);
+			}
+		}
+		
+		return resumen;
+	}
+
+
+	public ArrayList<Vacuna> getVacunasPendientes() {
+		ArrayList<Vacuna> pendientes = new ArrayList<>();
+
+		for (Vacuna vacuna : this.registroVacunacion.keySet()) {
+			Boolean estaAplicada = this.registroVacunacion.get(vacuna);
+			
+			if (estaAplicada == false) {
+				pendientes.add(vacuna);
+			}
+		}
+		return pendientes;
+	}
+
+
+	public ArrayList<Vacuna> getVacunasAplicadas() {
+		ArrayList<Vacuna> aplicadas = new ArrayList<>();
+		for (Vacuna vacuna : this.registroVacunacion.keySet()) {
+			Boolean estaAplicada = this.registroVacunacion.get(vacuna);
+
+			if (estaAplicada == true) {
+				aplicadas.add(vacuna);
+			}
+		}
+		return aplicadas;
+	}
 	
-}	
+
+	private boolean registroContieneVacuna(Vacuna vacuna) {
+		return this.registroVacunacion.containsKey(vacuna);
+	}
+
+}
---
+++ ProyectoFinalPOO/src/logico/SistemaGestion.java
@@ -0,0 +1,149 @@
+package logico;
+import java.util.ArrayList;
+
+public class SistemaGestion {
+	private ArrayList<Paciente> listaPacientes;
+	private ArrayList<Medico> listaMedicos;
+	private ArrayList<Cita> listaCitas;
+	private ArrayList<Enfermedad> catalogoEnfermedades;
+	private ArrayList<Vacuna> catalogoVacunas;
+	private ArrayList<Usuario> listaUsuarios;
+	private ArrayList<Secretaria> listaSecretarias;
+	private static SistemaGestion clinica = null;
+
+	private SistemaGestion() {
+		super();
+		listaPacientes = new ArrayList<>();
+		listaMedicos = new ArrayList<>();
+		listaCitas = new ArrayList<>();
+		catalogoEnfermedades = new ArrayList<>();
+		catalogoVacunas = new ArrayList<>();
+		listaUsuarios = new ArrayList<>();
+		listaSecretarias = new ArrayList<>();
+		if (listaUsuarios.isEmpty()) {
+			Usuario adminDefault = new Usuario("admin", "admin", "ADMIN", "admin-001");
+			listaUsuarios.add(adminDefault);
+		}
+	}
+
+	public static SistemaGestion getInstance(){
+		if(clinica == null){
+			clinica = new SistemaGestion();
+		}
+		return clinica;
+	}
+
+	public ArrayList<Paciente> getListaPacientes() {
+		return listaPacientes;
+	}
+	public void setListaPacientes(ArrayList<Paciente> listaPacientes) {
+		this.listaPacientes = listaPacientes;
+	}
+	public ArrayList<Medico> getListaMedicos() {
+		return listaMedicos;
+	}
+	public void setListaMedicos(ArrayList<Medico> listaMedicos) {
+		this.listaMedicos = listaMedicos;
+	}
+	public ArrayList<Cita> getListaCitas() {
+		return listaCitas;
+	}
+	public void setListaCitas(ArrayList<Cita> listaCitas) {
+		this.listaCitas = listaCitas;
+	}
+	public ArrayList<Enfermedad> getCatalogoEnfermedades() {
+		return catalogoEnfermedades;
+	}
+	public void setCatalogoEnfermedades(ArrayList<Enfermedad> catalogoEnfermedades) {
+		this.catalogoEnfermedades = catalogoEnfermedades;
+	}
+	public ArrayList<Vacuna> getCatalogoVacunas() {
+		return catalogoVacunas;
+	}
+	public void setCatalogoVacunas(ArrayList<Vacuna> catalogoVacunas) {
+		this.catalogoVacunas = catalogoVacunas;
+	}
+	public ArrayList<Usuario> getListaUsuarios() {
+		return listaUsuarios;
+	}
+	public void setListaUsuarios(ArrayList<Usuario> listaUsuarios) {
+		this.listaUsuarios = listaUsuarios;
+	}
+	public ArrayList<Secretaria> getListaSecretarias() {
+		return listaSecretarias;
+	}
+	public void setListaSecretarias(ArrayList<Secretaria> listaSecretarias) {
+		this.listaSecretarias = listaSecretarias;
+	}
+
+	public Usuario validarLogin(String username, String password) {
+		for (Usuario user : listaUsuarios) {
+			if (user.getUsername().equals(username)) {
+				if (user.VerifPass(password)) {
+					return user; 
+				} else {
+					return null; 
+				}
+			}
+		}
+		return null;
+	}
+
+	public void registrarPaciente(Paciente pac) {
+		listaPacientes.add(pac);
+	}
+	public void registrarMedico(Medico med) {
+		listaMedicos.add(med);
+	}
+	public void agregarEnfermedadCatalogo(Enfermedad enf) {
+		catalogoEnfermedades.add(enf);
+	}
+	public void agregarVacuna(Vacuna vac) {
+		catalogoVacunas.add(vac);
+	}
+	public Cita buscarCitaPorId(String idCita) {
+		Cita aux = null;
+		boolean encontrado = false;
+		int i = 0;
+		while(!encontrado && i < listaCitas.size()) {
+			if(listaCitas.get(i).getId().equalsIgnoreCase(idCita)) {
+				aux = listaCitas.get(i);
+				encontrado = true;
+			}
+			i++;
+			
+		}		
+		return aux;
+	}
+	public Paciente buscarPacientePorId(String idPaciente) {
+		Paciente aux = null;
+		boolean encontrado = false;
+		int i = 0;
+		while(!encontrado && i < listaPacientes.size()) {
+			if(listaPacientes.get(i).getId().equalsIgnoreCase(idPaciente)) {
+				aux = listaPacientes.get(i);
+				encontrado = true;
+			}
+			i++;
+		}
+		return aux;
+	}
+	public Medico buscarMedicoPorId(String idMedico) {
+		Medico aux = null;
+		boolean encontrado = false;
+		int i = 0;
+		while(!encontrado && i < listaMedicos.size()) {
+			if(listaMedicos.get(i).getId().equalsIgnoreCase(idMedico)) {
+				aux = listaMedicos.get(i);
+				encontrado = true;
+			}
+			i++;
+		}
+		return aux;
+	}
+	public Cita crearCita(String idVisitante, String idMedico) {
+		Cita aux = null;
+		
+		return aux;
+	}
+}
