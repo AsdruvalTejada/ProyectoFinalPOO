cebfd7294f6c3e30dd83985629366d22e56c93e5
Dell <Dell@DESKTOP-BKAVMK7>
2025-11-12 20:53:12 -0400

    Arreglo de Medico y Paciente, creación de Secretaria. Login basico

--- ProyectoFinalPOO/src/visual/Principal.java
+++ ProyectoFinalPOO/src/visual/Principal.java
@@ -1,5 +1,24 @@
 package visual;
 
-public class Principal {
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Principal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
 
 }
