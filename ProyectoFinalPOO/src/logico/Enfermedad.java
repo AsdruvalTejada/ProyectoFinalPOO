86dad9a3091f47eb8bd861f9351a55101b3da060
Wilmary Hernandez <Wilmary Hernandez@DESKTOP-1HPVPUQ>
2025-11-13 22:29:35 -0400

    Agregué los métodos equals() y hashCode() en las clases: Persona Vacuna Enfermedad Cita Consulta

--- ProyectoFinalPOO/src/logico/Enfermedad.java
+++ ProyectoFinalPOO/src/logico/Enfermedad.java
@@ -30,5 +30,31 @@ public class Enfermedad {
 	public void setEstaBajoVigilancia(Boolean estaBajoVigilancia) {
 		this.estaBajoVigilancia = estaBajoVigilancia;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Enfermedad other = (Enfermedad) obj;
+		
+		if (id == null) {
+			return other.id == null;
+		} else {
+			return id.equals(other.id);
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 		
 }
