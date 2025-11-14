83922f0f72d225a50caaaa3f7a08c24bef29c43a
Wilmary Hernandez <WSHH0001@ce.pucmm.edu.do>
2025-11-13 22:29:35 -0400

    Agregué los métodos equals() y hashCode() en las clases: Persona Vacuna Enfermedad Cita Consulta

--- ProyectoFinalPOO/src/logico/Cita.java
+++ ProyectoFinalPOO/src/logico/Cita.java
@@ -71,4 +71,30 @@ public class Cita {
 		return valido;
 	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Cita other = (Cita) obj;
+		
+		if (id == null) {
+			return other.id == null;
+		} else {
+			return id.equals(other.id);
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
 }
--- ProyectoFinalPOO/src/logico/Persona.java
+++ ProyectoFinalPOO/src/logico/Persona.java
@@ -74,4 +74,30 @@ public abstract class Persona {
 		}
 		return Period.between(this.fechaNacimiento, LocalDate.now()).getYears();
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Persona other = (Persona) obj;
+		
+		if (id == null) {
+			return other.id == null;
+		} else {
+			return id.equals(other.id);
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 }	
